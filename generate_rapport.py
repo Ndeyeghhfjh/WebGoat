59b34a5f0eda259fb678b8f5d6dd8148e9967fc4
Ndeyeghhfjh <ndeyeastoudieng@esp.sn>
2026-06-09 05:18:36 +0000

    fix: ajout generate_rapport.py pour export CSV

---
+++ generate_rapport.py
@@ -0,0 +1,20 @@
+﻿import json, os
+
+issues = []
+for f in ["result1.json", "result2.json"]:
+    try:
+        with open(f) as fp:
+            issues.extend(json.load(fp).get("issues", []))
+    except Exception:
+        pass
+
+with open("rapport_webgoat.csv", "w") as out:
+    out.write("Severity,Message,File,Line\n")
+    for i in issues:
+        msg = i.get("message", "").replace(",", ";")
+        sev = i.get("severity", "")
+        comp = i.get("component", "")
+        line = str(i.get("line", ""))
+        out.write(sev + "," + msg + "," + comp + "," + line + "\n")
+
+print("Rapport genere : " + str(len(issues)) + " issues")
